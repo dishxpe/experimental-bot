a21433df65c996f70cf54931dcca8efd0200bb54
Jasper <82205545+dishxpe@users.noreply.github.com>
2022-01-26 20:01:11 +0800

    Add files via upload

---
+++ commands/fun/dababy.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch")
+const Discord = require("discord.js")
+const link = 'https://www.reddit.com/r/dababyhentai.json?sort=top&t=week'
+
+module.exports = {
+    name : 'dababy',
+    description : "Drops a random meme from r/dababyhentai.",
+    run : async (bot,message,args) => {
+    let fetchMemes = await fetch(link).then(m => m.json())
+    const getMemes = fetchMemes.data.children;
+    let randomMeme = getMemes[Math.floor(Math.random() * getMemes.length)]
+    let memeEmbed = new Discord.MessageEmbed()
+    .setTitle(randomMeme.data.title)
+    .setImage(randomMeme.data.url)
+    .setColor('RANDOM');
+
+    message.channel.send(memeEmbed)
+
+    }
+}
