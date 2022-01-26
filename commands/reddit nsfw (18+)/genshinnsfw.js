3970a2faf69f615b6ce784f8ab475c029c8ba605
Jasper <82205545+dishxpe@users.noreply.github.com>
2022-01-26 20:08:31 +0800

    Add files via upload

---
+++ commands/reddit nsfw (18+)/genshinnsfw.js
@@ -0,0 +1,20 @@
+const fetch = require("node-fetch")
+const Discord = require("discord.js")
+const link = 'https://www.reddit.com/r/GenshinImpactNSFW.json?sort=top&t=day'
+
+module.exports = {
+    name : 'nsfwgenshin',
+    description : "Drops a random picture from r/GenshinImpactNSFW",
+    run : async (bot,message,args) => {
+    let fetchMemes = await fetch(link).then(m => m.json())
+    const getMemes = fetchMemes.data.children;
+    let randomMeme = getMemes[Math.floor(Math.random() * getMemes.length)]
+    let memeEmbed = new Discord.MessageEmbed()
+    .setTitle(randomMeme.data.title)
+    .setImage(randomMeme.data.url)
+    .setColor('RANDOM');
+
+    message.channel.send(memeEmbed)
+
+    }
+}
